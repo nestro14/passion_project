771a47028a5564beaa8cff448fb513f24e7b0d9f
Nestor Ceron <nestor_ceron@yahoo.com>
2017-09-04 22:36:26 -0700

    Add CSS and partials

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,3 +2,9 @@ get '/teams' do
   @teams = Team.all
   erb :'teams/index'
 end
+
+get '/teams/:id' do
+  @team = Team.find(params[:id])
+  @top_ten_players = Ranking.where(team: @team.code).order(nerd_rank: :ASC).limit(10)
+  erb :'teams/show'
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,29 +1,5 @@
 $(document).ready(function() {
-  // sortByPosition();
   $("a.nav-link").click(function() {
-  $( this ).toggleClass( "active" );
+    $( this ).toggleClass( "active" );
+  });
 });
-// $( ".nav-link" ).toggle(
-//   function() {
-//     $( this ).addClass( "active" );
-//   }, function() {
-//     $( this ).removeClass( "active" );
-//   }
-// );
-  // var sortByPosition = function() {
-  //   $("a").toggle(function() {
-  //   $(this).addClass('active');
-  // }, function() {
-  //   $(this).removeClass('active');
-  // });
-});
-
-
-//  var addClass = function () { $( ".nav-link" ).toggle(
-//   function() {
-//     $( this ).addClass( "active" );
-//   }, function() {
-//     $( this ).removeClass( "active" );
-//   }
-// );
-// }
